2f470c1a30610a686ea6810c499931aafbbe3c90
daraGotGitted <daraemwebdev@proton.me>
2024-11-06 04:35:37 +0000

    update

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const textarea = document.getElementById("text").children[0]
 const menuBar = document.getElementById("menuBar")
 const footer = document.getElementById("footer")
+const container = document.getElementById("container")
 
 let menuBarWidth = 50
 let footerHeight = 25
 
-menuBar.style.width = `${menuBarWidth}px`
-footer.style.height = `${footerHeight}px`
+function adjustSize(menuBarWidth, footerHeight) {
+    menuBar.style.width = `${menuBarWidth}px`
+    footer.style.height = `${footerHeight}px`
+    
+    textarea.style.width = `calc(100% - ${menuBarWidth}px)`
+    textarea.style.height = `calc(100% - ${footerHeight}px)`
+    textarea.style.transform = `translateX(${menuBarWidth}px)`
+}
 
-textarea.style.width = `calc(100% - ${menuBarWidth}px)`
-textarea.style.height = `calc(100% - ${footerHeight}px)`
-textarea.style.transform = `translateX(${menuBarWidth}px)`
+adjustSize(menuBarWidth, footerHeight)
 
+document.getElementById("buttonExpand").addEventListener("click", () => {
+    if (menuBarWidth > 50) {
+        container.style.display = "flex"
+        container.style.marginTop = "10px"
+        container.style.marginLeft = ""
+        menuBarWidth = 50
+    } else {
+        container.style.display = "block"
+        container.style.marginTop = "10px"
+        container.style.marginLeft = "calc(100% - 35px)"
+        menuBarWidth = 100
+    }
+
+    adjustSize(menuBarWidth, footerHeight)
+})
